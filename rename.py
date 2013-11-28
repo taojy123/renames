9aa08e739d4006782ac0868fc255e314b7688621
Tao Jiayuan <taojy123@163.com>
2013-11-28 15:19:11 +0800

    init

---
+++ rename.py
@@ -0,0 +1,27 @@
+# -*- coding: cp936 -*-
+import os
+
+print "本脚本将自动读取当前目录下的 a.csv 文件"
+print "然后根据 a.csv 中的数据对当前文件夹内文件批量重命名"
+raw_input("按下回车键开始")
+
+f = open("a.csv", "r")
+open("error.txt", "w")
+for line in f:
+    names = line.split(",")
+    n1 = names[0].strip()
+    n2 = names[1].strip()
+    
+    try:
+        os.rename(n1, n2)
+        print n1, "-->", n2
+    except:
+        print n1, "  X"
+        open("error.txt", "a").write(n1 + "\n")
+
+f.close()
+
+raw_input("完成")
+
+
+
