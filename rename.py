b32df2f3d87b840cd4f92113aee98cb249a03c2b
Tao Jiayuan <taojy123@163.com>
2013-12-05 17:54:23 +0800

    。。

--- rename.py
+++ rename.py
@@ -1,6 +1,11 @@
 # -*- coding: cp936 -*-
 import os
+import shutil
 
+if not os.path.exists("failure/"):
+    os.makedirs("failure/")
+
+            
 print "本脚本将自动读取当前目录下的 a.csv 文件"
 print "然后根据 a.csv 中的数据对当前文件夹内文件批量重命名"
 raw_input("按下回车键开始")
@@ -18,6 +23,11 @@ for line in f:
     except:
         print n1, "  X"
         open("error.txt", "a").write(n1 + "\n")
+
+        try:
+            shutil.copyfile(n1, "failure/" + n2)
+        except:
+            pass
 
 f.close()
 
